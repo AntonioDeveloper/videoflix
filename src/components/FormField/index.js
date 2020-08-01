b346f869b719a2cbae4849fc7d617e2d98a07870
AntonioDeveloper <juniorgabrielsantana@hotmail.com>
2020-07-31 22:33:01 -0300

    Melhorias no Formulário de cadastro de Categorias

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function FormField({ rows, cols, label, name, type, value, onChange }) {
+let FormField = function ({ rows, cols, label, name, type, value, onChange }) {
   return (
     <div>
       <label>
@@ -18,5 +19,26 @@ function FormField({ rows, cols, label, name, type, value, onChange }) {
     </div>
   )
 }
+
+
+
+FormField = styled.input`
+color: #141414;
+font-size: 1.2rem;
+display: flex;
+flex-direction: column;
+width: 50%;
+height: 58px;
+border-radius: 5px;
+transition: border-color 0.3s ease 0s;
+background: #ccc;
+outline: 0px;
+padding: 16px;
+margin: 15px auto;
+
+&:focus {
+border-bottom-color: #2a7ae4;
+}
+`;
 
 export default FormField;
--- src/components/pages/cadastro/categoria.js
+++ src/components/pages/cadastro/categoria.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import PageDefault from '../../PageDefault';
-import FormField from '../../FormField'
-//import { createGlobalStyle } from 'styled-components';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
-
   const valoresIniciais = {
     nome: '',
     descricao: '',
     cor: '',
   }
-
   const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
+  const [values, setValues] = useState('');
+
 
   function setValue(chave, valor) {
+    // chave: nome, descricao, bla, bli
     setValues({
-      ...values, [chave]: valor,
+      ...values,
+      [chave]: valor, // nome: 'valor'
     })
   }
 
@@ -32,61 +32,86 @@ function CadastroCategoria() {
     <PageDefault>
       <h1>Cadastro de Categoria: {values.nome}</h1>
 
-      <form onSubmit={function handleSubmit(infosDoEvento) {
+      <form align="center" onSubmit={function handleSubmit(infosDoEvento) {
         infosDoEvento.preventDefault();
         setCategorias([
-          ...categorias, values
+          ...categorias,
+          values
         ]);
 
-        setValues(valoresIniciais);
-
+        setValues(valoresIniciais)
       }}>
 
         <FormField
-          label="Categoria"
-          name="name"
+          label="Nome da Categoria"
           type="text"
+          name="nome"
+          placeholder="Nome da categoria"
           value={values.nome}
           onChange={handleChange}
         />
 
         <FormField
-          label="Descrição"
-          name="descricao"
+          label="Descrição:"
           type="textarea"
-          rows="4"
-          cols="50"
+          name="descricao"
           value={values.descricao}
+          cols={50}
+          rows={4}
+          placeholder="Descrição"
           onChange={handleChange}
         />
+        {/* <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              value={values.descricao}
+              name="descricao"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
+        <h4 align="center">Cor:</h4>
         <FormField
           label="Cor"
-          name="name"
           type="color"
+          name="cor"
           value={values.cor}
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
+        {/* <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              value={values.cor}
+              name="cor"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
-        <button>
+        <button style={{ marginTop: "10px" }}>
           Cadastrar
         </button>
-
       </form>
+
 
       <ul>
         {categorias.map((categoria, indice) => {
           return (
-            <li key={`${categoria} ${indice}`}>
-              {categoria.nome}
-              {categoria.descricao}
-              {categoria.cor}
+            <li key={`${categoria}${indice}`} style={{ display: "flex" }} >
+              {categoria.nome}, {categoria.descricao},  <span style={{ background: categoria.cor, width: "100px", height: "20px", marginLeft: "5px" }}></span>
             </li>
+
           )
         })}
       </ul>
 
       <Link to="/">
-        Página inicial
+        Ir para home
       </Link>
     </PageDefault >
   )
